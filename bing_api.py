276a9af6f5b8961f73be5d11b35362a568fb0e57
Ruoran Wang <ruoran@promethost.com>
2012-03-12 20:25:27 -0500

    bing test

--- bing_api.py
+++ bing_api.py
@@ -38,29 +38,38 @@ def main():
     print start
     print end
 
-    request_url = "http://dev.virtualearth.net/REST/V1/Routes/{method}?wp.0={start}&wp.1={end}&timeType=Departure&dateTime=3:00:00PM&output=json&key=AupvjJj-8-1RYDPSIIxnP8IQfomRdF8CJAyhe4KrWMBNS7pdOkUDRBU2OtlBoPu8".format(method=METHODS[method], start=start, end=end)
+    request_url = "http://dev.virtualearth.net/REST/V1/Routes/{method}?wp.0={start}&wp.1={end}&timeType=Departure&routePathOutput=Points&dateTime=3:00:00PM&optmz=distance&maxSolns=3&output=json&key=AupvjJj-8-1RYDPSIIxnP8IQfomRdF8CJAyhe4KrWMBNS7pdOkUDRBU2OtlBoPu8".format(method=METHODS[method], start=start, end=end)
+
+    google_url= "" 
 
     print request_url
     r = requests.get(request_url)
     
     raw = json.loads(r.text)
     
-    rjson = raw['resourceSets']
-    f = open('rjson.json', 'w')   
-    f.write(json.dumps(rjson, sort_keys=True, indent=2))
+    f = open('rjson.json', 'w')
+    f.write(json.dumps(raw, sort_keys=True, indent=2))
     f.close()
-    b = rjson[0]
-    legs = rjson[0]['resources'][0]['routeLegs'][0]['itineraryItems']
+    routes = raw['resourceSets'][0]['resources']
+    #routes = rjson[0]['resources']
+    for route in routes:
+        print route["id"] 
+        legs = route['routeLegs'][0]['itineraryItems']
+        parts = []
+        for leg in legs:
+            part = []
+            if leg['transitTerminus']:
+                info = "Terminus is " + str(leg['transitTerminus'])
+            else:
+                info = ""
 
-    part = []
-    parts = []
-    for leg in legs:
-        part = leg['instruction']['text'], leg['travelDistance']
-        parts.append(part)
+            part = leg['instruction']['text'], leg['travelDistance'], info, leg['iconType'] 
+            parts.append(part)
 
-    for part in parts:
-        print "Info: ", part[0]
-        print "Distance: ", part[1]
+        for part in parts:
+            print part[0]
+            print "Distance: ", part[1]
+        print "----------\n" 
 
     if options.print_flag:
         prettify(legs)
